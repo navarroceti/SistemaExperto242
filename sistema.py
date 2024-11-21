19f3b2d0eb829935ce7fa3837a60d58f1032dc2a
Jose Ramon Navarro Marquez <Jose.Navarro.Marquez@ibm.com>
2024-11-21 08:30:26 -0600

    Codigo base sistema experto

---
+++ sistema.py
@@ -0,0 +1,90 @@
+from enum import Enum
+
+class TokenType(Enum):
+    Y = 'y'
+    O = 'o'
+    SI = 'si'
+    ENTONCES = 'entonces'
+    NOT = 'no'
+    EOL = 'eol'
+    PALABRA = ''
+    
+
+class Token:
+    def __init__(self, tipo, valor):
+        self.tipo = tipo
+        self.valor = valor
+
+class Operador:
+    def __init__(self, tipo, valor):
+        self.tipo = tipo
+        self.valor = valor
+        
+class Proposicion:
+    def __init__(self, listaTokens):
+        self.listaTokens = listaTokens
+        self.posicion = 0
+    def ObtenerFrase(self):
+        return ' '.join(token.valor for token in self.listaTokens)
+            
+
+def main():
+    # Solicta al que ingrese una proposicion
+    print("Ingrese una proposicion: ")
+    proposicion = input()
+    
+    # Convertir la proposicion a simbolos y verificar que sea una proposicion valida
+    tokens = proposicion.lower().split(' ')
+    print("\nTokens:", tokens)
+    
+    # crear una lista y guardar el tipo de token y el valor
+    listaTokens = []
+    for token in tokens:
+        if token == 'y':
+            listaTokens.append(Token(TokenType.Y, token))
+        elif token == 'o':
+            listaTokens.append(Token(TokenType.O, token))
+        elif token == 'entonces':
+            listaTokens.append(Token(TokenType.ENTONCES, token))
+        elif token == 'no':
+            listaTokens.append(Token(TokenType.NOT, token))
+        else:
+            listaTokens.append(Token(TokenType.PALABRA, token))
+    listaTokens.append(Token(TokenType.EOL, ''))
+    
+    # Imprimir la lista de tokens
+    print("\nLista de tokens:")
+    print("Tipo\tValor")
+    for token in listaTokens:
+        print(token.tipo, token.valor)
+        
+    # Recorre los elementos y agrupa toda las palabras consecutivas en una lista
+    listaProposiciones = []
+    listaPalabras = []
+    for token in listaTokens:
+        if token.tipo == TokenType.PALABRA:
+            listaPalabras.append(token)
+        else:
+            if len(listaPalabras) > 0:
+                listaProposiciones.append(Proposicion(listaPalabras))
+                listaPalabras = []
+                
+    # Imprime la lista de proposiciones
+    print("\nLista de proposiciones:")
+    for proposicion in listaProposiciones:
+        print(proposicion.ObtenerFrase())
+        
+        
+    # Crear un conjunto para que no se repitan las proposiciones
+    conjuntoProposiciones = set()
+    for proposicion in listaProposiciones:
+        conjuntoProposiciones.add(proposicion.ObtenerFrase())
+        
+    # Imprimir el conjunto de proposiciones
+    print("\nConjunto de proposiciones:")
+    for proposicion in conjuntoProposiciones:
+        print(proposicion)
+       
+    
+if __name__ == "__main__":
+    main()
